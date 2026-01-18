483675fafe50e4c1dd9374823affc102686a1cc2
Ravindu Samarasinghe <124066949+Ravindu0117@users.noreply.github.com>
2026-01-19 00:20:33 +0530

    uploaded alll files

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Vue + Laravel is Live 🚀</h1>
+    <p>If you see this, your frontend is working perfectly.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 100px;
+  font-family: Arial, sans-serif;
+}
+</style>
